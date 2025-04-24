b019f7067216f7f5fd9df1a9702a564cf73f14a1
Salvador De La Torre <144974636+sdelatorre02@users.noreply.github.com>
2025-04-23 20:00:25 -0700

    Add files via upload
    Added doc_similarity.py to find similarities between resume and job description.

---
+++ doc_similarity.py
@@ -0,0 +1,27 @@
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from nltk.tokenize import word_tokenize
+import nltk
+
+nltk.download("punkt")
+
+class ResumeDoc2VecAnalyzer:
+    def __init__(self):
+        self.model = None
+
+    def prepare_documents(self, job_desc, resume_text):
+        documents = [
+            TaggedDocument(words=word_tokenize(job_desc.lower()), tags=["JOB"]),
+            TaggedDocument(words=word_tokenize(resume_text.lower()), tags=["RESUME"])
+        ]
+        return documents
+
+    def train_model(self, documents):
+        model = Doc2Vec(vector_size=50, window=2, min_count=1, workers=4, epochs=40)
+        model.build_vocab(documents)
+        model.train(documents, total_examples=model.corpus_count, epochs=model.epochs)
+        self.model = model
+
+    def compare_documents(self, job_desc, resume_text):
+        documents = self.prepare_documents(job_desc, resume_text)
+        self.train_model(documents)
+        return self.model.dv.similarity("JOB", "RESUME")
